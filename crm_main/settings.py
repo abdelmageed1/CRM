703817249b4397ac9d3497a47a13b1c6df5aac09
Abdelmageed <abdelmageed2511@gmail.com>
2026-05-24 15:53:38 +0300

    Fix CSRF verification failure by adding SECURE_PROXY_SSL_HEADER

--- crm_main/settings.py
+++ crm_main/settings.py
@@ -13,11 +13,15 @@ DEBUG = 'RENDER' not in os.environ
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS = [RENDER_EXTERNAL_HOSTNAME]
-    CSRF_TRUSTED_ORIGINS = [f'https://{RENDER_EXTERNAL_HOSTNAME}']
+    ALLOWED_HOSTS = [RENDER_EXTERNAL_HOSTNAME, 'localhost', '127.0.0.1']
+    CSRF_TRUSTED_ORIGINS = [f'https://{RENDER_EXTERNAL_HOSTNAME}', 'https://*.onrender.com']
 else:
     ALLOWED_HOSTS = ['*']
     CSRF_TRUSTED_ORIGINS = []
+
+# Tell Django it is behind a proxy that sets X-Forwarded-Proto to https
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
